081a83ed0c09804408b536954e51dc51d8dc91de
Patrick S <dev@sotopatrick.com>
2024-09-18 22:39:30 +0200

    JWT auth security configured

--- src/main/kotlin/com/soquiz/plugins/security/Security.kt
+++ src/main/kotlin/com/soquiz/plugins/security/Security.kt
@@ -5,25 +5,27 @@ import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.util.logging.*
+import java.util.logging.Level
+import java.util.logging.Logger
 
 fun Application.configureSecurity() {
-    // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
-    authentication {
-        jwt {
-            realm = jwtRealm
+    val secretKey = environment.config.property("jwt.secret").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val realmText = environment.config.property("jwt.realm").getString()
+
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = realmText
             verifier(
-                JWT
-                    .require(Algorithm.HMAC256(jwtSecret))
-                    .withAudience(jwtAudience)
-                    .withIssuer(jwtDomain)
+                JWT.require(Algorithm.HMAC256(secretKey))
+                    .withIssuer(issuer)
+                    .withAudience(audience)
                     .build()
             )
             validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+                if (credential.payload.audience.contains(audience)) JWTPrincipal(credential.payload) else null
             }
         }
     }
