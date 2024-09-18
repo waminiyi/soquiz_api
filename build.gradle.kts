e1f4c9aa820bf203e4ad5c3f21baa1b65e70d527
Patrick S <dev@sotopatrick.com>
2024-09-18 13:27:47 +0200

    Initial set up

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.20"
+}
+
+group = "com.soquiz"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.postgresql)
+    implementation(libs.h2)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+
+    implementation("org.jetbrains.exposed:exposed-dao:0.53.0")
+    implementation("org.mindrot:jbcrypt:0.4")
+
+
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
---
+++ src/main/kotlin/com/soquiz/Application.kt
@@ -0,0 +1,15 @@
+package com.soquiz
+
+import com.soquiz.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureSecurity()
+    configureRouting()
+}
